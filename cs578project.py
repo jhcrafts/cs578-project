1db1d5a215e80f00859d8a7bb410663248166b07
frcssanson <frcs.sanson@gmail.com>
2015-10-21 12:40:32 -0400

    Add a few more statistics track

--- cs578project.py
+++ cs578project.py
@@ -8,7 +8,7 @@ import math
 import copy
 import random
 import json
-
+import numpy as np
 ### main
 ### ----
 ### The main program loop
@@ -32,19 +32,19 @@ import json
 ##
 ##def validateInput(args):
 ##    pass
-##    return 
+##    return
 ##
-##class Example:    
+##class Example:
 ##    'Class to encapsulate data examples'
 ##    featureValues = dict()
-##    
+##
 ##    def __init__(self, label):
 ##        self.label = label
 ##        self.featureValues = dict()
 ##
 ##    def label(self):
 ##        return self.label
-##        
+##
 ##    def addfeature(self, feature,value):
 ##        self.featureValues[feature] = value
 ##
@@ -62,32 +62,44 @@ import json
 ##
 def debugprint(string):
     print("DEBUG: " + str(string))
-    
+
 
 def main():
     # Read in the data file
     trainingdata = open("train.json")
     examples = json.load(trainingdata)
-
+    featuresubset=np.empty(20,dtype=object)
+    for i in range(len(featuresubset)):
+        featuresubset[i]=set()
+    cuisinedict={}
     featureset = set()
     labels = set()
     examplecount = 0
     for example in examples:
         examplecount += 1
-        labels.add(example["cuisine"])
+        if not(example["cuisine"] in labels):
+            labels.add(example["cuisine"])
+            cuisinedict[example["cuisine"]]=len(labels)-1
         for ingredient in example["ingredients"]:
-            featureset.add(ingredient)
-
-    print("Training Data Properties:")    
+            featuresubset[cuisinedict[example["cuisine"]]].add(ingredient)
+    for dummy in featuresubset:
+        featureset=featureset.union(dummy)
+    print("Training Data Properties:")
     print("Number of Examples (Recipes):            " + str(examplecount))
     print("Number of Features (Unique Ingredients): " + str(len(featureset)))
     print("Number of Labels (Cuisines):             " + str(len(labels)))
 
+    for cuisine in labels:
+        print "Size of the",cuisine,"cuisine:",len(featuresubset[cuisinedict \
+        [cuisine]])
+
+
     debugprint(labels)
+
     # ====================================
     # WRITE CODE FOR YOUR EXPERIMENTS HERE
     # ====================================
-  
+
 if __name__ == '__main__':
     main()
 
