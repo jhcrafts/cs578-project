ff147f5fa07ae977240722c68bf3a0f74dae088a
frcssanson <frcs.sanson@gmail.com>
2015-10-28 20:47:19 -0400

    add decomposdata function and start change subtree function

--- decision_tree.py
+++ decision_tree.py
@@ -64,39 +64,50 @@ def classify(decisionTree, example):
             currentnode=currentnode[2]
     return(currentnode)
 #%%
-def entropygain(dataset,attribute,t):
-    if dataset.shape[0]==0:
-        return(0)
-    else:
-        entropy=np.zeros(2)
-        positif=  np.sum(dataset[dataset[:,attribute]>t][:,-1])*1.
-        tot=len(dataset[dataset[:,attribute]>t][:,-1])*1.
-        negatif= tot-positif
-        entropy[1]= tot/len(dataset)*(positif/tot*np.log(tot/positif)+ \
-        negatif/tot*np.log(tot/negatif))
-        positif=  np.sum(dataset[dataset[:,attribute]<=t][:,-1])*1.
-        tot=len(dataset[dataset[:,attribute]<=t][:,-1])*1.
-        negatif= tot-positif
-        entropy[0]= tot/len(dataset)*(positif/tot*np.log(tot/positif)+ \
-        negatif/tot*np.log(tot/negatif))
-        entropy[np.isnan(entropy)]=0.
-        positif= np.sum(dataset[:,-1])*1.
-        tot=len(dataset[:,-1])*1.
-        negatif= tot-positif
-        gen_entropy=positif/tot*np.log(tot/positif)+ \
-        negatif/tot*np.log(tot/negatif)-np.sum(entropy)
-    return(gen_entropy)
+def decomposdata(examples):
+    """
+    Based on the examples, computes the frequency of every ingredient for
+    for every type of cuisine. It can be useful in order to compute entropy
+    the values are stored in numpy arrays
+    -featurefreq[cuisine][ingredient] = freq of that ingredient
+    indexed by
+    -dictionnary cuisinedict (type of cuisine :chinese mexican ...) returns
+    cuisine index
+    -dictionnaries featuresubset[i]=  the dictionnary of ingredients for
+    cuisine i  retunrs ingredient index
+    """
+    featuresubset=np.empty(20,dtype=object)
+    for i in range(len(featuresubset)):
+        featuresubset[i]=set()
+    cuisinedict={}
+    labels = set()
+    for example in examples:
+        if not(example["cuisine"] in labels): # to build the dictionnary index on the fly
+            labels.add(example["cuisine"])
+            cuisinedict[example["cuisine"]]=len(labels)-1
+        for ingredient in example["ingredients"]:
+            featuresubset[cuisinedict[example["cuisine"]]].add(ingredient)
+    featurefreq=np.empty(20,dtype=object)
+    for i,subset in enumerate(featuresubset):
+        featurefreq[i]=np.zeros(len(subset))
+        featuresubset[i]=listodict(list(subset))
+    for example in examples:
+        cuisine=cuisinedict[example["cuisine"]]
+        for ingredient in example["ingredients"]:
+            featurefreq[cuisine][featuresubset[cuisine][ingredient]]+=1.
+    return(featurefreq,featuresubset,cuisinedict)
 #%%
-def subtree(featurefreq,featuresubset,cuisinedict,cuisine,max_depth=9,mingain=0):
+def subtree(dataset,max_depth=9,mingain=0):
     if (featuresubset==[]):
         #make sure the dataset is not empty (I can't imagine this happening in practise)
-        return(dataset[:,-1][0])
+        return(featuresubset)
     else:
         tree=np.empty(4,dtype='object')
-        best_attribute,index_best,t,gain=best_choice(featurefreq,featuresubset,cuisinedict,cuisine)
+        featurefreq,featuresubset,cuisinedict=decomposdata(dataset)
+        best_attribute,index_best=best_choice(featurefreq,featuresubset,cuisinedict,cuisine)
         tree[0]=best_attribute
         #split the dataset
-        attributes=np.delete(attributes,index_best,0)
+
         temp=np.delete(np.arange(dataset.shape[1]),index_best,0)
         datasets=np.empty(2,dtype='object')
         datasets[0]=dataset[dataset[:,index_best]<=t][:,temp]
@@ -122,10 +133,11 @@ def searchdict(index,dic):
 #%%
 
 def best_choice(featurefreq,featuresubset,cuisinedict,cuisine):
-    entropy_gain,totalfeatures=entropy(featurefreq,featuresubset,cuisinedict)
-    index=np.argmin(entropy_gain[cuisinedict[cuisine]][frequency[cuisine][:]>20])
-    best_attribute=searchdict(index,totalfeatures)
-    return(best_attribute,index,t,gain)
+    entropy_gain,frequency=entropy(featurefreq,featuresubset,cuisinedict)
+    index=np.argmin(entropy_gain[cuisinedict[cuisine]][frequency[cuisinedict[cuisine]]>20])
+    best_attribute=searchdict(index,featuresubset[cuisinedict[cuisine]])
+    print(best_attribute)
+    return(best_attribute,index,)
 
 def random_choice(attributes,dataset,t):
     index_best=int(np.random.uniform(0,len(attributes)))
@@ -159,7 +171,10 @@ def threshold_optimize(dataset,attribute_index):
  #   print(t1)
     return(t1)
 #%%
-
+def split(featuresubset,cuisinedict,cuisine,ingredient):
+    for i,cuis in enumerate(cusisinedict):
+       del featuresubset[i][ingredient]
+    return (featuresubset,cuisinedict,cuisine,ingredient)
  #%%
 def majorityvote(featurefreq,featuresubset,cuisinedict,cuisine):
     #takes the majority vote of a set
